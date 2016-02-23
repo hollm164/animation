9ceabd1a58a11369ee4dd8425cb4760116aad3d6
hollm164 <marielleholland@Marielles-MacBook-Pro.local>
2016-02-23 11:39:35 -0500

    For this assignment I wanted to illustrate the natural darkness that can overtake our life paths but also the fact that we can choose the light and to be happy.

--- homework/homework.js
+++ homework/homework.js
@@ -2,14 +2,65 @@
   Homework
 */
 
-//declare your variables
+var posX;
+var posY;
+
+var x = 0;
+var y = 0;
 
 function setup(){
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(600,600);
+  background(0);
 
-  //set things up
+  posX = 0;
+  posY = 0;
+
 }
 
 function draw(){
-  //draw
+
+  fill(255);
+  if (mouseIsPressed)
+
+    ellipse(mouseX,mouseY,mouseY/5,mouseY/5);
+  else
+    fill(0);
+    stroke(255);
+    ellipse(mouseX,mouseY,mouseY/5,mouseY/5);
+
+  posX = posX + 1;
+  posY=posX;
+
+  noStroke();
+  fill(0,255,230);
+
+  ellipse(-posX+600,-posY+600,30,30);
+
+  ellipse(posX+130,posY,30,30);
+
+  ellipse(posX+160,posY,30,30);
+
+  ellipse(posX+190,posY,30,30);
+
+  ellipse(posX,posY+130,30,30);
+
+  ellipse(posX,posY+160,30,30);
+
+  ellipse(posX,posY+190,30,30);
+
+  ellipse(-posX+570,posY-30,30,30);
+
+  ellipse(-posX+630,posY+30,30,30);
+
+  ellipse(-posX+600,posY,30,30);
+
+  ellipse(posX-60,-posY+540,30,30);
+
+  ellipse(posX-90,-posY+510,30,30);
+
+  ellipse(posX+60,-posY+660,30,30);
+
+  ellipse(posX+90,-posY+690,30,30);
+
+
 }
